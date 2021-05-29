eccfcf4b817d9c124840d5fa2fce573efe64707a
Avirup Saha <sahaavirup1998@gmail.com>
2021-05-29 11:14:10 +0530

    react circle question

---
+++ src/circle.js
@@ -0,0 +1,20 @@
+import Shape from "./shape";
+
+
+class Circle extends Shape {
+
+  constructor(props) {
+
+    super(props);
+
+}
+
+  render() {
+
+    return <div>{this.calculateArea()}</div>;
+
+  }
+
+}
+
+export default Circle;
---
+++ src/shape.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class Shape extends React.Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = { color: "red" };
+
+  }
+
+  drawShape() {
+
+    return "Shape";
+
+  }
+
+  calculateArea() {
+
+    return "Area";
+
+  }
+
+  render() {
+
+    return (
+
+      <div>
+
+        Area:{this.calculateArea()} shape:{this.drawShape()}
+
+      </div>
+
+    );
+
+  }
+
+}
+
+export default Shape;
